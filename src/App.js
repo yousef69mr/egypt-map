207e6cfce706f474cbdac1a96d5a4c21d5b902c1
yousef69mr <84923007+yousef69mr@users.noreply.github.com>
2023-01-04 00:58:06 +0200

    2nd version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import EgyptMap from "./components/EgyptMap/EgyptMap";
 
 const App = () => {
+    const [search, setSearch] = useState('');
+    const [searchInfo, setSearchInfo] = useState([]);
+    const [results, setResults] = useState([]);
+
+    const handleSearch = async e => {
+        //  e.preventDefault();
+
+        const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`;
+
+        const response = await fetch(endpoint);
+
+        console.log(response)
+
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+
+        const json = await response.json();
+        console.log(json)
+        setResults(json.query.search);
+        setSearchInfo(json.query.searchinfo);
+    }
+
+    useEffect(() => {
+        handleSearch()
+    }, [])
+
     return (
         <div className="App">
             <EgyptMap />
--- src/components/EgyptMap/EgyptMap.jsx
+++ src/components/EgyptMap/EgyptMap.jsx
@@ -20,7 +20,7 @@ const EgyptMap = () => {
     );
     setHeight(window.innerHeight);
 
-    console.log(mapSize);
+    // console.log(mapSize);
   });
 
   useEffect(() => {
@@ -30,9 +30,19 @@ const EgyptMap = () => {
   return (
     <section
       className={css.container}
-      style={{height: Height < mapSize ? `${Height - 10}px` : `${Height}px` }}
+      style={{ height: Height < mapSize ? `${Height - 10}px` : `${Height}px` }}
     >
-      <Map map_size={mapSize} sectionHeight={Height} governorates={data.Governorates} />
+      <div className={css.side_text}>
+        <h1>Welcome To Egypt</h1>
+        <p>The Ancient Sezilization in the middle east</p>
+      </div>
+      <div className={css.map_container}>
+        <Map
+          map_size={mapSize}
+          sectionHeight={Height}
+          governorates={data.Governorates}
+        />
+      </div>
     </section>
   );
 };
